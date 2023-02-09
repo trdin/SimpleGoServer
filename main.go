426f06b6f5fdea9a248d5a57afc62690088e932a
Peter <trdin.peter@gmail.com>
2023-02-09 09:27:20 +0100

    saving people to array

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"log"
 	"net/http"
 )
+type Person struct {
+    name    string
+    address string
+}
+
+var people []Person
 
 func formHandler (w http.ResponseWriter, r *http.Request){
 	if err := r.ParseForm(); err != nil{
@@ -14,6 +20,11 @@ func formHandler (w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "POST request successful")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
+
+	person := Person{name: name, address: address}
+
+	people = append(people, person)
+
 	fmt.Fprintf(w, "Name = %s\n", name)
 	fmt.Fprintf(w, "Address = %s\n", address)
 }
@@ -29,12 +40,28 @@ func helloHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "hello!")
 }
 
+func peopleHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/people"{
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET"{
+		http.Error(w, "Method not supported", http.StatusMethodNotAllowed)
+	}
+
+	for i := 0; i < len(people); i++ {
+        fmt.Println(people[i].name + " " + people[i].address + "\n")
+		fmt.Fprintf(w, people[i].name + " " + people[i].address + "\n")
+    }
+}
+
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/people", peopleHandler)
 
 	fmt.Printf("Starting server on port 3001\n")
 	if err:= http.ListenAndServe(":3001", nil); err != nil {
